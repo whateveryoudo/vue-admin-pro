fdbb2182cbfdf3204580c62b853b33c888c23fab
whateveryoudo <1795261900@qq.com>
2019-08-19 20:23:18 +0800

    柱状图开始

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,6 +1,7 @@
 import ChartCard from './ChartCard/index.vue'
 import MiniArea from './MiniArea/index.vue'
 import MiniBar from './MiniBar/index.vue'
+import Bar from './Bar/index.vue'
 import MiniProgress from './MiniProgress/index.vue'
 
 
@@ -8,5 +9,6 @@ export {
     ChartCard,
     MiniArea,
     MiniBar,
-    MiniProgress
+    MiniProgress,
+    Bar
 }
--- src/views/basic-tpl/dashboard/analysis.vue
+++ src/views/basic-tpl/dashboard/analysis.vue
@@ -1,17 +1,36 @@
 <template>
     <div class="page-basic-layout analysis-container">
         <IntroduceRow></IntroduceRow>
-
+        <div class="sales-wrapper">
+            <el-tabs v-model="activeIndex" @tab-click="handleClick">
+                <el-tab-pane label="销售额" name="1">
+                    <Bar></Bar>
+                </el-tab-pane>
+                <el-tab-pane label="访问量" name="2">访问量</el-tab-pane>
+            </el-tabs>
+        </div>
     </div>
 
 </template>
 
 <script>
     import IntroduceRow from './components/IntroduceRow'
+    import {Bar} from '@/components/Charts'
     export default {
         name: "analysis",
+        data(){
+            return {
+                activeIndex : '1'
+            }
+        },
         components : {
-            IntroduceRow
+            IntroduceRow,
+            Bar
+        },
+        methods : {
+            handleClick(tab, event){
+
+            }
         }
     }
 </script>
